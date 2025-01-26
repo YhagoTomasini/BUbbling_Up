7ab46f2c35db8cec883910782edea3f8ff694bbe
Hugo Soares <hhsoares112@gmail.com>
2025-01-25 21:19:10 -0300

    fixed bubble timer

--- bubble/scripts/bubble.gd
+++ bubble/scripts/bubble.gd
@@ -3,7 +3,14 @@ extends CharacterBody2D
 @export_subgroup("Nodes")
 @export var floatComponent: FloatComponent
 
+@onready var timer = $Timer
+
+func _ready() -> void:
+	$Timer.start(1)
+
 func _physics_process(delta: float) -> void:
 	floatComponent.handleFloat(self, delta)
-	
 	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	queue_free()
