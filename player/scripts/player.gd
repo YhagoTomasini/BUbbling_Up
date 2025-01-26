d81eb02196231e60e51739e4d70d9ac7b0900c9d
YhagoTomasini <148384672+YhagoTomasini@users.noreply.github.com>
2025-01-26 10:31:36 -0300

    anim gatito funcionando

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -2,20 +2,37 @@ extends CharacterBody2D
 class_name Player
 
 @export var gravity = 500
-@export var speed = 150
+@export var speed = 100
 var jump_force = 250
+
+@onready var anim = $Sprite2D
 
 func _physics_process(delta):
 	if is_on_floor() == false:
 		velocity.y += gravity * delta
+	
+	elif velocity.x != 0:
+		anim.play("Walk")
 		
 	if Input.is_action_just_pressed("jump") && is_on_floor():
 		jump(jump_force)
 		
 	if velocity.y >= 500:
 		velocity.y = 500
+		
+	if velocity.y > 0:
+		anim.play("Falling")
+	elif velocity.y < 0:
+		anim.play("Jump")
+	
 	var direction = Input.get_axis("left", "right")
 	velocity.x = direction  * speed 
+	
+	if direction:
+		anim.scale.x = -1 if direction < 0 else 1
+	elif is_on_floor() == true:
+		anim.play("Idle")
+	
 	move_and_slide()
 	
 func jump(force):
