ddda7e5f8a168975e5417f975678ab56a0620ff8
Miguel Teixeira <81261687+CookieNinjaBR@users.noreply.github.com>
2025-01-26 13:38:16 -0300

    added wind effect to bubble
    c:

--- bubble/scripts/bubble.gd
+++ bubble/scripts/bubble.gd
@@ -5,21 +5,32 @@ extends CharacterBody2D
 
 @onready var timer = $Timer
 
+var wind_force = 0  
+var wind_direction = 0  
+
 func _ready() -> void:
 	$Timer.start(1)
 
 func _physics_process(delta: float) -> void:
 	floatComponent.handleFloat(self, delta)
+
+	if wind_direction != 0:
+		velocity.x += wind_force * wind_direction * delta
+
 	move_and_slide()
 
 func _on_timer_timeout() -> void:
 	queue_free()
 
-
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
-		queue_free()  
+		queue_free()
 	if area.is_in_group("wind"):
-		var is_wind_left = area.get("direction") 
-		var wind_direction = -1 if is_wind_left else 1
-		velocity.x += wind_direction * 100
+		var is_wind_left = area.get("is_wind_left") 
+		wind_direction = 1 if is_wind_left else -1
+		wind_force = 500  
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.is_in_group("wind"):
+		wind_direction = 0
+		wind_force = 0
